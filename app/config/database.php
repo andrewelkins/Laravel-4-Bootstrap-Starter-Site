0e313ba1f0a7938ffb3d56b446dac50c3aeac863
Andrew13 <andrew13@github.com>
2013-02-06 16:15:37 -0800

    Update db creds for project and gitignore vendor dir

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'laravelbootstrapempowered',
+			'database'  => 'laravelbootstrapstarter',
 			'username'  => 'user',
 			'password'  => 'password',
 			'charset'   => 'utf8',
