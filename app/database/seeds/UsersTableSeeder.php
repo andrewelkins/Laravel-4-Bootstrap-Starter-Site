42604f38bfd66530f951ff14f407dbc6a9dbd262
andrew13 <github@andrewelkins.com>
2013-02-17 18:08:14 -0800

    Updating seeder for confirmation code error.

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -12,13 +12,19 @@ class UsersTableSeeder extends Seeder {
                 'username'      => 'admin',
                 'email'      => 'admin@test.com',
                 'password'   => Hash::make('admin'),
-                'confirmed'   => 0
+                'confirmed'   => 0,
+                'confirmation_code' => md5(microtime().Config::get('app.key')),
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
             ),
             array(
                 'username'      => 'user',
                 'email'      => 'user@test.com',
                 'password'   => Hash::make('user'),
-                'confirmed'   => 0
+                'confirmed'   => 0,
+                'confirmation_code' => md5(microtime().Config::get('app.key')),
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime,
             )
         );
 
