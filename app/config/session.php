3649e6a7ea423319744fea44bacdd2c76a878dd7
andrew13 <github@andrewelkins.com>
2013-03-14 11:45:47 -0700

    update for l4 session file

--- app/config/session.php
+++ app/config/session.php
@@ -83,6 +83,19 @@ return array(
 
 	'lottery' => array(2, 100),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Session Cookie Domain
+    |--------------------------------------------------------------------------
+    |
+    | Here you may change the domain of the cookie used to identify a session
+    | in your application. This will determine which domains the cookie is
+    | available to in your application. A sensible default has been set.
+    |
+    */
+
+    'domain' => null,
+
 	/*
 	|--------------------------------------------------------------------------
 	| Session Cookie Name
