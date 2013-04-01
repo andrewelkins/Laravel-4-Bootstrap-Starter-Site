40244fb46a4048d901ef2a6f6e052aeb2672733e
Andrew Elkins <andrew@andrewelkins.com>
2013-03-31 18:14:08 -0700

    Clean up formatting. 4space instead of 8, no tabs.

--- app/config/app.php
+++ app/config/app.php
@@ -2,44 +2,44 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Application Debug Mode
-	|--------------------------------------------------------------------------
-	|
-	| When your application is in debug mode, detailed error messages with
-	| stack traces will be shown on every error that occurs within your
-	| application. If disabled, a simple generic error page is shown.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
 
-	'debug' => true,
+    'debug' => true,
 
-	/*
-	|--------------------------------------------------------------------------
-	| Application Timezone
-	|--------------------------------------------------------------------------
-	|
-	| Here you may specify the default timezone for your application, which
-	| will be used by the PHP date and date-time functions. We have gone
-	| ahead and set this to a sensible default for you out of the box.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    |
+    */
 
-	'timezone' => 'UTC',
+    'timezone' => 'UTC',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Application Locale Configuration
-	|--------------------------------------------------------------------------
-	|
-	| The application locale determines the default locale that will be used
-	| by the translation service provider. You are free to set this value
-	| to any of the locales which will be supported by the application.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Application Locale Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The application locale determines the default locale that will be used
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
+    |
+    */
 
-	'locale' => 'en',
+    'locale' => 'en',
 
     /*
     |--------------------------------------------------------------------------
@@ -54,32 +54,32 @@ return array(
 
     'locales' => array(),
 
-	/*
-	|--------------------------------------------------------------------------
-	| Encryption Key
-	|--------------------------------------------------------------------------
-	|
-	| This key is used by the Illuminate encrypter service and should be set
-	| to a random, long string, otherwise these encrypted values will not
-	| be safe. Make sure to change it before deploying any application!
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Encryption Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the Illuminate encrypter service and should be set
+    | to a random, long string, otherwise these encrypted values will not
+    | be safe. Make sure to change it before deploying any application!
+    |
+    */
 
-	'key' => 'YourSecretKey!!!',
+    'key' => 'YourSecretKey!!!',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Autoloaded Service Providers
-	|--------------------------------------------------------------------------
-	|
-	| The service providers listed here will be automatically loaded on the
-	| request to your application. Feel free to add your own services to
-	| this array to grant expanded functionality to your applications.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
 
-	'providers' => array(
-		/* Laravel Base Providers */
+    'providers' => array(
+        /* Laravel Base Providers */
         'Illuminate\Foundation\Providers\ArtisanServiceProvider',
         'Illuminate\Auth\AuthServiceProvider',
         'Illuminate\Cache\CacheServiceProvider',
@@ -113,86 +113,86 @@ return array(
         'Illuminate\View\ViewServiceProvider',
         'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		/* Additional Providers */
-		'Zizaco\Confide\ConfideServiceProvider', // Confide Provider
+        /* Additional Providers */
+        'Zizaco\Confide\ConfideServiceProvider', // Confide Provider
         'Zizaco\Entrust\EntrustServiceProvider', // Entrust Provider for roles
         'ExpressiveDateServiceProvider', // Expressive Date
         'Basset\BassetServiceProvider', // Better Asset Management
         'BigElephant\Presenter\PresenterServiceProvider', // Presenter
-	'Way\Generators\GeneratorsServiceProvider', // Generators
+        'Way\Generators\GeneratorsServiceProvider', // Generators
 
-	),
+    ),
 
-	/*
-	|--------------------------------------------------------------------------
-	| Service Provider Manifest
-	|--------------------------------------------------------------------------
-	|
-	| The service provider manifest is used by Laravel to lazy load service
-	| providers which are not needed for each request, as well to keep a
-	| list of all of the services. Here, you may set its storage spot.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Service Provider Manifest
+    |--------------------------------------------------------------------------
+    |
+    | The service provider manifest is used by Laravel to lazy load service
+    | providers which are not needed for each request, as well to keep a
+    | list of all of the services. Here, you may set its storage spot.
+    |
+    */
 
-	'manifest' => storage_path().'/meta',
+    'manifest' => storage_path() . '/meta',
 
-	/*
-	|--------------------------------------------------------------------------
-	| Class Aliases
-	|--------------------------------------------------------------------------
-	|
-	| This array of class aliases will be registered when this application
-	| is started. However, feel free to register as many as you wish as
-	| the aliases are "lazy" loaded so they don't hinder performance.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Class Aliases
+    |--------------------------------------------------------------------------
+    |
+    | This array of class aliases will be registered when this application
+    | is started. However, feel free to register as many as you wish as
+    | the aliases are "lazy" loaded so they don't hinder performance.
+    |
+    */
 
-	'aliases' => array(
-		/* Laravel Base Aliases */
-        'App'             => 'Illuminate\Support\Facades\App',
-        'Artisan'         => 'Illuminate\Support\Facades\Artisan',
-        'Auth'            => 'Illuminate\Support\Facades\Auth',
-        'Blade'           => 'Illuminate\Support\Facades\Blade',
-        'Cache'           => 'Illuminate\Support\Facades\Cache',
-        'ClassLoader'     => 'Illuminate\Support\ClassLoader',
-        'Config'          => 'Illuminate\Support\Facades\Config',
-        'Controller'      => 'Illuminate\Routing\Controllers\Controller',
-        'Cookie'          => 'Illuminate\Support\Facades\Cookie',
-        'Crypt'           => 'Illuminate\Support\Facades\Crypt',
-        'DB'              => 'Illuminate\Support\Facades\DB',
-        'Eloquent'        => 'Illuminate\Database\Eloquent\Model',
-        'Event'           => 'Illuminate\Support\Facades\Event',
-        'File'            => 'Illuminate\Support\Facades\File',
-        'Form'            => 'Illuminate\Support\Facades\Form',
-        'Hash'            => 'Illuminate\Support\Facades\Hash',
-        'Html'            => 'Illuminate\Support\Facades\Html',
-        'Input'           => 'Illuminate\Support\Facades\Input',
-        'Lang'            => 'Illuminate\Support\Facades\Lang',
-        'Log'             => 'Illuminate\Support\Facades\Log',
-        'Mail'            => 'Illuminate\Support\Facades\Mail',
-        'Paginator'       => 'Illuminate\Support\Facades\Paginator',
-        'Password'        => 'Illuminate\Support\Facades\Password',
-        'Queue'           => 'Illuminate\Support\Facades\Queue',
-        'Redirect'        => 'Illuminate\Support\Facades\Redirect',
-        'Redis'           => 'Illuminate\Support\Facades\Redis',
-        'Request'         => 'Illuminate\Support\Facades\Request',
-        'Response'        => 'Illuminate\Support\Facades\Response',
-        'Route'           => 'Illuminate\Support\Facades\Route',
-        'Schema'          => 'Illuminate\Support\Facades\Schema',
-        'Seeder'          => 'Illuminate\Database\Seeder',
-        'Session'         => 'Illuminate\Support\Facades\Session',
-        'Str'             => 'Illuminate\Support\Str',
-        'URL'             => 'Illuminate\Support\Facades\URL',
-        'Validator'       => 'Illuminate\Support\Facades\Validator',
-        'View'            => 'Illuminate\Support\Facades\View',
-		/* Additional Aliases */
-		'Confide'    	  => 'Zizaco\Confide\ConfideFacade', // Confide Alias
-        'Entrust'         => 'Zizaco\Entrust\EntrustFacade', // Entrust Alias
-        'Presenter'       => 'BigElephant\Presenter\Presenter', // Presenter
-        'Presentable'     => 'BigElephant\Presenter\PresentableInterface', // Presenter
-        'Basset'          => 'Basset\Facades\Basset', // Better Asset Management
-        'String'          => 'Andrew13\Helpers\String', // Better Asset Management
+    'aliases' => array(
+        /* Laravel Base Aliases */
+        'App' => 'Illuminate\Support\Facades\App',
+        'Artisan' => 'Illuminate\Support\Facades\Artisan',
+        'Auth' => 'Illuminate\Support\Facades\Auth',
+        'Blade' => 'Illuminate\Support\Facades\Blade',
+        'Cache' => 'Illuminate\Support\Facades\Cache',
+        'ClassLoader' => 'Illuminate\Support\ClassLoader',
+        'Config' => 'Illuminate\Support\Facades\Config',
+        'Controller' => 'Illuminate\Routing\Controllers\Controller',
+        'Cookie' => 'Illuminate\Support\Facades\Cookie',
+        'Crypt' => 'Illuminate\Support\Facades\Crypt',
+        'DB' => 'Illuminate\Support\Facades\DB',
+        'Eloquent' => 'Illuminate\Database\Eloquent\Model',
+        'Event' => 'Illuminate\Support\Facades\Event',
+        'File' => 'Illuminate\Support\Facades\File',
+        'Form' => 'Illuminate\Support\Facades\Form',
+        'Hash' => 'Illuminate\Support\Facades\Hash',
+        'Html' => 'Illuminate\Support\Facades\Html',
+        'Input' => 'Illuminate\Support\Facades\Input',
+        'Lang' => 'Illuminate\Support\Facades\Lang',
+        'Log' => 'Illuminate\Support\Facades\Log',
+        'Mail' => 'Illuminate\Support\Facades\Mail',
+        'Paginator' => 'Illuminate\Support\Facades\Paginator',
+        'Password' => 'Illuminate\Support\Facades\Password',
+        'Queue' => 'Illuminate\Support\Facades\Queue',
+        'Redirect' => 'Illuminate\Support\Facades\Redirect',
+        'Redis' => 'Illuminate\Support\Facades\Redis',
+        'Request' => 'Illuminate\Support\Facades\Request',
+        'Response' => 'Illuminate\Support\Facades\Response',
+        'Route' => 'Illuminate\Support\Facades\Route',
+        'Schema' => 'Illuminate\Support\Facades\Schema',
+        'Seeder' => 'Illuminate\Database\Seeder',
+        'Session' => 'Illuminate\Support\Facades\Session',
+        'Str' => 'Illuminate\Support\Str',
+        'URL' => 'Illuminate\Support\Facades\URL',
+        'Validator' => 'Illuminate\Support\Facades\Validator',
+        'View' => 'Illuminate\Support\Facades\View',
+        /* Additional Aliases */
+        'Confide' => 'Zizaco\Confide\ConfideFacade', // Confide Alias
+        'Entrust' => 'Zizaco\Entrust\EntrustFacade', // Entrust Alias
+        'Presenter' => 'BigElephant\Presenter\Presenter', // Presenter
+        'Presentable' => 'BigElephant\Presenter\PresentableInterface', // Presenter
+        'Basset' => 'Basset\Facades\Basset', // Better Asset Management
+        'String' => 'Andrew13\Helpers\String', // Better Asset Management
 
-	),
+    ),
 
 );
