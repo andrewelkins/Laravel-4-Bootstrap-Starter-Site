98211ae44d857ebb964429de226227aba7912eb3
Andrew <github@andrewelkins.com>
2013-04-29 23:35:42 -0700

    Fixes #46 Thanks @filitchp

--- app/views/admin/blogs/edit.blade.php
+++ app/views/admin/blogs/edit.blade.php
@@ -95,7 +95,7 @@ Blog Post Update ::
 	<!-- Form Actions -->
 	<div class="control-group">
 		<div class="controls">
-			<a class="btn btn-link" href="{{{ URL::to('admin/blog') }}}">Cancel</a>
+			<a class="btn btn-link" href="{{{ URL::to('admin/blogs') }}}">Cancel</a>
 			<button type="reset" class="btn">Reset</button>
 			<button type="submit" class="btn btn-success">Publish</button>
 		</div>
