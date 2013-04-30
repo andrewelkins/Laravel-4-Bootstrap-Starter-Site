b50afc324a5b17b0a5fd47d87cff015cae54fefd
Andrew <github@andrewelkins.com>
2013-04-30 08:00:45 -0700

    Fixes #48

--- app/views/admin/layouts/default.blade.php
+++ app/views/admin/layouts/default.blade.php
@@ -57,16 +57,16 @@
 						</a>
 						<div class="nav-collapse collapse">
 							<ul class="nav">
-								<li{{{ (Request::is('admin') ? ' class="active"' : '') }}}><a href="{{{ URL::to('admin') }}}"><i class="icon-home icon-white"></i> Home</a></li>
-								<li{{{ (Request::is('admin/blogs*') ? ' class="active"' : '') }}}><a href="{{{ URL::to('admin/blogs') }}}"><i class="icon-list-alt icon-white"></i> Blog</a></li>
-								<li{{{ (Request::is('admin/comments*') ? ' class="active"' : '') }}}><a href="{{{ URL::to('admin/comments') }}}"><i class="icon-bullhorn icon-white"></i> Comments</a></li>
+								<li{{{ (Request::is('admin') ? ' active' : '') }}}><a href="{{{ URL::to('admin') }}}"><i class="icon-home icon-white"></i> Home</a></li>
+								<li{{{ (Request::is('admin/blogs*') ? ' active' : '') }}}><a href="{{{ URL::to('admin/blogs') }}}"><i class="icon-list-alt icon-white"></i> Blog</a></li>
+								<li{{{ (Request::is('admin/comments*') ? ' active' : '') }}}><a href="{{{ URL::to('admin/comments') }}}"><i class="icon-bullhorn icon-white"></i> Comments</a></li>
 								<li class="dropdown{{{ (Request::is('admin/users*|admin/roles*') ? ' active' : '') }}}">
 									<a class="dropdown-toggle" data-toggle="dropdown" href="{{{ URL::to('admin/users') }}}">
 										<i class="icon-user icon-white"></i> Users <span class="caret"></span>
 									</a>
 									<ul class="dropdown-menu">
-										<li{{{ (Request::is('admin/users*') ? ' class="active"' : '') }}}><a href="{{{ URL::to('admin/users') }}}"><i class="icon-user"></i> Users</a></li>
-										<li{{{ (Request::is('admin/roles*') ? ' class="active"' : '') }}}><a href="{{{ URL::to('admin/roles') }}}"><i class="icon-user"></i> Roles</a></li>
+										<li{{{ (Request::is('admin/users*') ? ' active' : '') }}}><a href="{{{ URL::to('admin/users') }}}"><i class="icon-user"></i> Users</a></li>
+										<li{{{ (Request::is('admin/roles*') ? ' active' : '') }}}><a href="{{{ URL::to('admin/roles') }}}"><i class="icon-user"></i> Roles</a></li>
 									</ul>
 								</li>
 							</ul>
