6e668a91bfe0f7297d2de66052c454236d3a1a7f
Andrew Elkins <github@andrewelkins.com>
2013-05-08 10:43:51 -0700

    Fixes #60

--- app/lang/en/user/user.php
+++ app/lang/en/user/user.php
@@ -10,12 +10,13 @@ return array(
 	|
 	*/
 
-	'register'          => 'Register',
-	'login'             => 'Login',
-	'account'           => 'Account',
-	'forgot_password'   => 'Forgot Password',
-	'settings'          => 'Settings',
-	'profile'          => 'Profile',
+	'register'              => 'Register',
+	'login'                 => 'Login',
+	'login_first'           => 'Login first',
+	'account'               => 'Account',
+	'forgot_password'       => 'Forgot Password',
+	'settings'              => 'Settings',
+	'profile'               => 'Profile',
 	'user_account_is_not_confirmed'          => 'User Account is not confirmed.',
 
 );
