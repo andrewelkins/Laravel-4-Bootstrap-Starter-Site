f6da72d50d65b7b96d410e4cbc32506f9fdf888e
Andrew Elkins <github@andrewelkins.com>
2013-05-10 16:36:37 -0700

    updating unit test WIP

--- app/database/migrations/2013_02_08_031702_entrust_setup_tables.php
+++ app/database/migrations/2013_02_08_031702_entrust_setup_tables.php
@@ -25,6 +25,8 @@ class EntrustSetupTables extends Migration {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('role_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
