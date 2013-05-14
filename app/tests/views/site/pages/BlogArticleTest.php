9d76a1080a270bc8cd1937ce2391ca82c4383481
Andrew Elkins <github@andrewelkins.com>
2013-05-14 11:36:05 -0700

    Test article page display

--- app/tests/views/site/pages/BlogArticleTest.php
+++ app/tests/views/site/pages/BlogArticleTest.php
@@ -1,19 +1,30 @@
 <?php
 
-class BlogTest extends BaseControllerTestCase {
+class BlogArticleTest extends BaseControllerTestCase {
 
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testIndexResponse()
-    {
-        $crawler = $this->client->request('GET', '/');
-
-        $this->assertTrue($this->client->getResponse()->isOk());
-    }
-
-
+//    /**
+//     * A basic functional test example.
+//     *
+//     * @return void
+//     */
+//    public function testIndexResponse()
+//    {
+//        $crawler = $this->client->request('GET', '/vivendo-suscipiantur-vim-te-vix');
+//
+//        $this->assertTrue($this->client->getResponse()->isOk());
+//    }
+//    /**
+//     * A basic functional test example.
+//     *
+//     * @return void
+//     */
+//    public function testCommentCountResponse()
+//    {
+//        $crawler = $this->client->request('GET', '/vivendo-suscipiantur-vim-te-vix');
+//
+//        $this->assertCount(1, $crawler->filter('h4:contains("2 Comments")'));
+//    }
+//
+//
 
 }
