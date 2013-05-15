50aa41a7c961ed74447f9e50a94539e66fa8add5
Andrew <github@andrewelkins.com>
2013-05-15 10:27:50 -0600

    Update 2013_02_05_044505_create_comments_table.php

--- app/database/migrations/2013_02_05_044505_create_comments_table.php
+++ app/database/migrations/2013_02_05_044505_create_comments_table.php
@@ -14,6 +14,7 @@ class CreateCommentsTable extends Migration {
 		// Create the `Comments` table
 		Schema::create('comments', function($table)
 		{
+            		$table->engine = 'InnoDB';
 			$table->increments('id')->unsigned();
 			$table->integer('user_id')->unsigned();
 			$table->integer('post_id')->unsigned();
