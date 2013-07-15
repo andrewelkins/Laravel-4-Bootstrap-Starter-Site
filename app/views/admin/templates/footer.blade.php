68926a3259de51b35d74918f42e7a8737e1ee92b
Andrew Elkins <github@andrewelkins.com>
2013-07-14 17:12:02 -0700

    Add footer template

---
+++ app/views/admin/templates/footer.blade.php
@@ -0,0 +1,5 @@
+<div id="footer">
+  <div class="container">
+    <span><p class="muted credit">Powered By <a href="https://github.com/andrew13/Laravel-4-Bootstrap-Starter-Site">Laravel 4 Starter Site</a></p></span>
+  </div>
+</div>
