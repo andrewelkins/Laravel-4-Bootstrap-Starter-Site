ffd877c677ffa4a8aaa779d3b60f3f3b47e4c6d1
Andrew Elkins <github@andrewelkins.com>
2014-02-13 11:34:51 -0800

    Finishing merge

--- app/config/testing/session.php
+++ app/config/testing/session.php
@@ -8,17 +8,10 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| This option controls the default session "driver" that will be used on
-<<<<<<< HEAD
-	| requets. By default, we will use the light-weight cookie driver but
-	| you may specify any of the other wonderful drivers provided here.
-	|
-	| Supported: "cookie", file", "database", "apc",
-=======
 	| requests. By default, we will use the lightweight native driver but
 	| you may specify any of the other wonderful drivers provided here.
 	|
 	| Supported: "native", "cookie", "database", "apc",
->>>>>>> e72b12092986c2ff70a616eebd3d2a9675ee5b92
 	|            "memcached", "redis", "array"
 	|
 	*/
