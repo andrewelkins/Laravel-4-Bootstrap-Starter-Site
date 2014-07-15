f7f9f92f6fcbcf96eae68447b747738dc4542d43
Hasan <hasandegismez@gmail.com>
2014-07-15 18:36:57 +0300

    Fix #342 - wysiwyg-color.css 404 Not Found Error

--- public/assets/js/wysihtml5/bootstrap-wysihtml5.js
+++ public/assets/js/wysihtml5/bootstrap-wysihtml5.js
@@ -450,7 +450,7 @@
                 "pre": 1
             }
         },
-        stylesheets: ["./lib/css/wysiwyg-color.css"], // (path_to_project/lib/css/wysiwyg-color.css)
+        stylesheets: [], //CSS stylesheets to load - (path_to_project/lib/css/wysiwyg-color.css)
         locale: "en"
     };
 
