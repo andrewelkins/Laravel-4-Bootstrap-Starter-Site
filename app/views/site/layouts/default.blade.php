86baabf3a7dc36a87330619b3a1910a19489e9ab
Andrew Elkins <github2@andrewelkins.com>
2014-08-20 11:36:57 -0700

    Fixes #350

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -72,7 +72,7 @@
                         <li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
                         @else
                         <li {{ (Request::is('user/login') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/login') }}}">Login</a></li>
-                        <li {{ (Request::is('user/register') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
+                        <li {{ (Request::is('user/create') ? ' class="active"' : '') }}><a href="{{{ URL::to('user/create') }}}">{{{ Lang::get('site.sign_up') }}}</a></li>
                         @endif
                     </ul>
 					<!-- ./ nav-collapse -->
