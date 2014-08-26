afa24382cba3fef0abb4d6f8dcdbcb59a16b16f4
Anew_G. <breakitwall@gmail.com>
2014-08-26 18:17:15 +0800

    Update default.blade.php
    add some section

--- app/views/site/layouts/default.blade.php
+++ app/views/site/layouts/default.blade.php
@@ -9,10 +9,15 @@
 			Laravel 4 Sample Site
 			@show
 		</title>
+		@section('meta_keywords')
 		<meta name="keywords" content="your, awesome, keywords, here" />
+		@show
+		@section('meta_author')
 		<meta name="author" content="Jon Doe" />
+		@show
+		@section('meta_description')
 		<meta name="description" content="Lorem ipsum dolor sit amet, nihil fabulas et sea, nam posse menandri scripserit no, mei." />
-
+                @show
 		<!-- Mobile Specific Metas
 		================================================== -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
