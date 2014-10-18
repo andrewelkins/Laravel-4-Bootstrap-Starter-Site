4e420032554f1ee1ac299195d31150619226fa14
Andrew Elkins <andrewelkins@users.noreply.github.com>
2014-10-18 14:16:00 -0700

    Update start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-    'local' => array('VirtualMint'), // Change this to your local machine hostname.
+    'local' => array('VirtualMint','homestead','*.local'), // Change this to your local machine hostname.
     'staging' => array('your-staging-machine-name'),
     'production' => array('your-production-machine-name'),
 
