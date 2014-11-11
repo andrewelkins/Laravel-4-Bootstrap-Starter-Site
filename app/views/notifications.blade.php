985c455275b31db4511833df78e6d06070dd0b89
John <ceesco53@gmail.com>
2014-11-10 21:13:54 -0500

    correct the comparison operators

--- app/views/notifications.blade.php
+++ app/views/notifications.blade.php
@@ -6,7 +6,7 @@
 </div>
 @endif
 
-@if ($message = Session::get('success'))
+@if ($message == Session::get('success'))
 <div class="alert alert-success alert-block">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
 	<h4>Success</h4>
@@ -20,7 +20,7 @@
 </div>
 @endif
 
-@if ($message = Session::get('error'))
+@if ($message == Session::get('error'))
 <div class="alert alert-danger alert-block">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
 	<h4>Error</h4>
@@ -34,7 +34,7 @@
 </div>
 @endif
 
-@if ($message = Session::get('warning'))
+@if ($message == Session::get('warning'))
 <div class="alert alert-warning alert-block">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
 	<h4>Warning</h4>
@@ -48,7 +48,7 @@
 </div>
 @endif
 
-@if ($message = Session::get('info'))
+@if ($message == Session::get('info'))
 <div class="alert alert-info alert-block">
 	<button type="button" class="close" data-dismiss="alert">&times;</button>
 	<h4>Info</h4>
